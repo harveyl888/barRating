f0f924ecddc4a8cc0050011d989e1a29ef9012c1
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-05-24 21:07:42 -0400

    skeleton

---
+++ R/barRating.R
@@ -0,0 +1,51 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+barRating <- function(message, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'barRating',
+    x,
+    width = width,
+    height = height,
+    package = 'barRating'
+  )
+}
+
+#' Shiny bindings for barRating
+#'
+#' Output and render functions for using barRating within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a barRating
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name barRating-shiny
+#'
+#' @export
+barRatingOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'barRating', width, height, package = 'barRating')
+}
+
+#' @rdname barRating-shiny
+#' @export
+renderBarRating <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, barRatingOutput, env, quoted = TRUE)
+}
