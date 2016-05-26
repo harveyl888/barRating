49a827984a14d14121196b1e4243e5b2bc7c2907
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-05-25 20:38:25 -0400

    simple example

---
+++ inst/htmlwidgets/examples/shiny_example_01/app.R
@@ -0,0 +1,22 @@
+## Example
+
+library(shiny)
+library(barRating)
+
+server <- function(input, output) {
+  output$example1 <- renderBarRating(barRating('example'))
+}
+
+ui <- fluidPage(
+
+  HTML('<select id="example">'),
+  HTML('<option value="1">1</option>'),
+  HTML('<option value="2">2</option>'),
+  HTML('<option value="3">3</option>'),
+  HTML('<option value="4">4</option>'),
+  HTML('<option value="5">5</option>'),
+  HTML('</select>'),
+  barRatingOutput('example1')
+)
+
+shinyApp(ui = ui, server = server)
